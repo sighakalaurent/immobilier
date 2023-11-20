2263ea3f03647170fa85aeb38142c99eeb54f0e3
sighakalaurent <nguifosighaka>
2023-11-20 08:30:29 +0100

    formulaire

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function SignIn() {
   return (
-    <div>Sign In</div>
+    <section>
+      <h1 className='text-3xl text-center mt-6 font-bold'>Connexion</h1>
+      <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto '>
+      <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
+        <img src='https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fHww' alt='key'
+        className='w-full rounded-2xl'/>
+      </div>
+<div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'> 
+  <form > 
+    <input className='w-full' type='email' id='email' ></input>
+  </form>
+</div>
+      </div>
+    </section>
   )
 }
